146cd81124e93c1cf28f4ac040875181362dd09f
Fèlix Vinyals <felix@felixvinyals.com>
2019-04-02 15:32:04 +0200

    Fix

--- TalkToMe.cpp
+++ TalkToMe.cpp
@@ -26,54 +26,58 @@ void TalkToMe::findEndOfCommand() {
   // Keeps storing chars in the array until ETX or CrLf is detected
 
   byte index; // An index to move in the loops
-  byte charactersOnRecivingBuffer = 0; // How many characters (bytes) do we have in the reciving buffer
+  //byte charactersOnRecivingBuffer = 0; // How many characters (bytes) do we have in the reciving buffer
   char serialRecivedCharacter = 0; // Where we store what we read from the buffer
   
-  charactersOnRecivingBuffer = Serial.available();
-  Serial.println("Serial.available(): " + String(charactersOnRecivingBuffer));
-  if (charactersOnRecivingBuffer > 0) { // There is something in the buffer
-    for (index = 0; index < charactersOnRecivingBuffer; index++) { // Let's check all what we've recived
-      serialRecivedCharacter = Serial.read(); // Get a character from the buffer
-      bytesRecived++; // One more byte recived
-      
-      if (bytesRecived >= serialReciveBufferSize) { // Buffer overflow error
-        Serial.println("Buffer overflow");
-        clearSerialRecivingBuffer();
-      }
-      
-      if (serialRecivedCharacter == -1) { // The buffer is empty
-        Serial.println("Nothing on the buffer");
-      }
-      else if (serialRecivedCharacter == 13) { // We got the Carriage Return character
-        lastCharIsCR = true;
-      }
-      else if (serialRecivedCharacter == 2) { // The first recived byte is STX
-        clearSerialRecivingBuffer();
+  //charactersOnRecivingBuffer = Serial.available();
+  
+  //if (charactersOnRecivingBuffer > 0) { // There is something in the buffer
+    //for (index = 0; index < charactersOnRecivingBuffer; index++) { // Let's check all what we've recived
+	serialRecivedCharacter = Serial.read(); // Get a character from the buffer
+	while(serialRecivedCharacter != -1) {
+	  
+	  bytesRecived++; // One more byte recived
+	  
+	  if (bytesRecived >= serialReciveBufferSize) { // Buffer overflow error
+		Serial.println("Buffer overflow");
+		clearSerialRecivingBuffer();
+	  }
+	  
+	  if (serialRecivedCharacter == -1) { // The buffer is empty
+		Serial.println("Nothing on the buffer");
+	  }
+	  else if (serialRecivedCharacter == 13) { // We got the Carriage Return character
+		lastCharIsCR = true;
+	  }
+	  else if (serialRecivedCharacter == 2) { // The first recived byte is STX
+		clearSerialRecivingBuffer();
 		recivedSTX = true;
-      }
-      
-      // End of command is CrLf
-      else if ((serialRecivedCharacter == 10) && (lastCharIsCR == true)) {
+	  }
+	  
+	  // End of command is CrLf
+	  else if ((serialRecivedCharacter == 10) && (lastCharIsCR == true)) {
 		analyzeCommand();
 		clearSerialRecivingBuffer();
-      }
-      // End of command is ETX
-      else if ((serialRecivedCharacter == 3) && (recivedSTX == true)) {
+		return;
+	  }
+	  // End of command is ETX
+	  else if ((serialRecivedCharacter == 3) && (recivedSTX == true)) {
 		analyzeCommand();
 		clearSerialRecivingBuffer();
-      }
+		return;
+	  }
 
-      else {
-        lastCharIsCR = false;
-        for (index = 0; index < serialReciveBufferSize; index++) { // Let's move through the array to find the first empty space
-          if (serialRecivingBufferArray[index] == 0) { // Empty space found
-            serialRecivingBufferArray[index] = serialRecivedCharacter; // Fill the empty space
-            break; // Stop the for
-          }
-        }
-      }
-    }
-  }
+	  else {
+		lastCharIsCR = false;
+		for (index = 0; index < serialReciveBufferSize; index++) { // Let's move through the array to find the first empty space
+		  if (serialRecivingBufferArray[index] == 0) { // Empty space found
+			serialRecivingBufferArray[index] = serialRecivedCharacter; // Fill the empty space
+			break; // Stop the for
+		  }
+		}
+	  }
+	serialRecivedCharacter = Serial.read(); // Get a character from the buffer
+	}
 }
 
 void TalkToMe::analyzeCommand() {
